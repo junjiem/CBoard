efad0260526ddf62bb441bec34d9c4caa7eaed7a
jiekong <13770677221@139.com>
2017-08-10 15:07:47 +0800

    百度地图修改

--- src/main/webapp/org/cboard/service/chart/chartScatterMapBmapService.js
+++ src/main/webapp/org/cboard/service/chart/chartScatterMapBmapService.js
@@ -75,118 +75,104 @@ cBoard.service('chartScatterMapBmapService', function () {
         // 地图自定义样式
         var bmap = {
             center: [startPoint.x, startPoint.y],
-            // zoom: 5,
+            zoom: 5,
             roam: true,
             mapStyle: {
                 styleJson: [{
-                    "featureType": "water",
-                    "elementType": "all",
-                    "stylers": {
-                        "color": "#044161"
+                    'featureType': 'water',
+                    'elementType': 'all',
+                    'stylers': {
+                        'color': '#d1d1d1'
                     }
                 }, {
-                    "featureType": "land",
-                    "elementType": "all",
-                    "stylers": {
-                        "color": "#004981"
+                    'featureType': 'land',
+                    'elementType': 'all',
+                    'stylers': {
+                        'color': '#f3f3f3'
                     }
                 }, {
-                    "featureType": "boundary",
-                    "elementType": "geometry",
-                    "stylers": {
-                        "color": "#064f85"
+                    'featureType': 'railway',
+                    'elementType': 'all',
+                    'stylers': {
+                        'visibility': 'off'
                     }
                 }, {
-                    "featureType": "railway",
-                    "elementType": "all",
-                    "stylers": {
-                        "visibility": "off"
+                    'featureType': 'highway',
+                    'elementType': 'all',
+                    'stylers': {
+                        'color': '#fdfdfd'
                     }
                 }, {
-                    "featureType": "highway",
-                    "elementType": "geometry",
-                    "stylers": {
-                        "color": "#004981"
+                    'featureType': 'highway',
+                    'elementType': 'labels',
+                    'stylers': {
+                        'visibility': 'off'
                     }
                 }, {
-                    "featureType": "highway",
-                    "elementType": "geometry.fill",
-                    "stylers": {
-                        "color": "#005b96",
-                        "lightness": 1
+                    'featureType': 'arterial',
+                    'elementType': 'geometry',
+                    'stylers': {
+                        'color': '#fefefe'
                     }
                 }, {
-                    "featureType": "highway",
-                    "elementType": "labels",
-                    "stylers": {
-                        "visibility": "off"
+                    'featureType': 'arterial',
+                    'elementType': 'geometry.fill',
+                    'stylers': {
+                        'color': '#fefefe'
                     }
                 }, {
-                    "featureType": "arterial",
-                    "elementType": "geometry",
-                    "stylers": {
-                        "color": "#004981"
+                    'featureType': 'poi',
+                    'elementType': 'all',
+                    'stylers': {
+                        'visibility': 'off'
                     }
                 }, {
-                    "featureType": "arterial",
-                    "elementType": "geometry.fill",
-                    "stylers": {
-                        "color": "#00508b"
+                    'featureType': 'green',
+                    'elementType': 'all',
+                    'stylers': {
+                        'visibility': 'off'
                     }
                 }, {
-                    "featureType": "poi",
-                    "elementType": "all",
-                    "stylers": {
-                        "visibility": "off"
+                    'featureType': 'subway',
+                    'elementType': 'all',
+                    'stylers': {
+                        'visibility': 'off'
                     }
                 }, {
-                    "featureType": "green",
-                    "elementType": "all",
-                    "stylers": {
-                        "color": "#056197",
-                        "visibility": "off"
+                    'featureType': 'manmade',
+                    'elementType': 'all',
+                    'stylers': {
+                        'color': '#d1d1d1'
                     }
                 }, {
-                    "featureType": "subway",
-                    "elementType": "all",
-                    "stylers": {
-                        "visibility": "off"
+                    'featureType': 'local',
+                    'elementType': 'all',
+                    'stylers': {
+                        'color': '#d1d1d1'
                     }
                 }, {
-                    "featureType": "manmade",
-                    "elementType": "all",
-                    "stylers": {
-                        "visibility": "off"
+                    'featureType': 'arterial',
+                    'elementType': 'labels',
+                    'stylers': {
+                        'visibility': 'off'
                     }
                 }, {
-                    "featureType": "local",
-                    "elementType": "all",
-                    "stylers": {
-                        "visibility": "off"
+                    'featureType': 'boundary',
+                    'elementType': 'all',
+                    'stylers': {
+                        'color': '#fefefe'
                     }
                 }, {
-                    "featureType": "arterial",
-                    "elementType": "labels",
-                    "stylers": {
-                        "visibility": "off"
+                    'featureType': 'building',
+                    'elementType': 'all',
+                    'stylers': {
+                        'color': '#d1d1d1'
                     }
                 }, {
-                    "featureType": "boundary",
-                    "elementType": "geometry.fill",
-                    "stylers": {
-                        "color": "#029fd4"
-                    }
-                }, {
-                    "featureType": "building",
-                    "elementType": "all",
-                    "stylers": {
-                        "color": "#1a5787"
-                    }
-                }, {
-                    "featureType": "label",
-                    "elementType": "all",
-                    "stylers": {
-                        "visibility": "off"
+                    'featureType': 'label',
+                    'elementType': 'labels.text.fill',
+                    'stylers': {
+                        'color': '#999999'
                     }
                 }]
             }
