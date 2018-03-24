c36501da451cac6a9c8dd90ad1751716b679a1fc
Junjie.M <junjie.miao@goupwith.com>
2018-03-24 12:28:49 +0800

    update

--- CSD/scripts/control.sh
+++ CSD/scripts/control.sh
@@ -9,6 +9,14 @@ TIMESTAMP=$(date)
 
 CMD=$1
 
+# Preference order:
+# 1. CBOARD_HOME (set by cboard_env.sh in the CBoard parcel).
+# 2. CDH_CBOARD_HOME (set by cdh_env.sh in the CDH parcel).
+# 3. Hardcoded default value (where the Cloudera packages install CBoard).
+DEFAULT_CBOARD_HOME=/usr/lib/cboard
+export CBOARD_HOME=${CBOARD_HOME:-$CDH_CBOARD_HOME}
+export CBOARD_HOME=${CBOARD_HOME:-$DEFAULT_CBOARD_HOME}
+
 echo "CMD: $CMD"
 echo "SERVER_WEB_PORT: $SERVER_WEB_PORT"
 
